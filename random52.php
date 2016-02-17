890d1c52db434521c33b1239ce535a445fc108d6
IT5706021631042 <5706021631042@fitm.kmutnb.ac.th>
2016-02-17 23:49:07 +0700

    incomplete1

---
+++ random52.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<mete charset="utf-8">
+<title>Random</title></head>
+
+<style type="text/css">
+body {
+background-image: url(00115209);
+background-attachment:fixed;
+background-repeat: no-repeat; }
+}
+</style>
+
+<body>
+<table  align="center" width="600" bgcolor="#EEDC82">
+  <br><br><br>
+
+  <tr>
+  <td width="300" height="60" colspan="2" align="center">
+  <h2><br>Random 1-52</h2>
+  </td>
+
+
+
+
+
+
+</table>
+</form>
+</body>
+</html>
