c613759c9e1e49732f4d80409ff3a14e46f21083
IT5706021631042 <5706021631042@fitm.kmutnb.ac.th>
2016-02-24 11:22:49 +0700

    ok

---
+++ Lab612.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+ <head>
+   <title>การสร้างฟอร์มในการรับค่าเพื่อการตาราง</title>
+     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+  <form method="post" action="lab612.php">
+<table align="center" border="1" width="400" bgcolor="#E6E6FA">
+  <tr>
+    <td colspan="2" align="center">ทดสอบการใช้ Control Structure</td>
+    <tr>
+      <tr>
+        <td align="right">Enter Row</td>
+        <td><input type="text" name="row" size="15" value=""/></td>
+      </tr>
+        <td align="right">Enter Col</td>
+        <td><input type="text" name="col" size="15" value=""/></td>
+      </tr>
+      <tr>
+        <td align="center" colspan="2">
+          <input type="submit" value="OK"/>
+          <input type="reset" value="CLEAR"/>
+        </td>
+      </tr>
+      <br><br><br><br>
+    </table>
+  </form>
+<?php
+$row = $_POST['row'];
+$col = $_POST['col'];
+echo "<table align='center' border='4' width='300'>";
+for ($r=1; $r <= $row; $r++){
+echo "<tr>";
+for ($c=1; $c <= $col; $c++){
+if ($r==$c){
+echo "<td align='center'>";
+echo "$r,$c</td>";
+}
+else
+{
+echo "<td align='center'> $r,$c </td>";
+}
+}
+echo "</tr>";
+}
+echo "</table>";
+?>
+ </body>
+</html>
