21b498abbb21ccbe6783f5c9b943bf2148913c05
IT5706021631042 <5706021631042@fitm.kmutnb.ac.th>
2016-02-24 10:37:12 +0700

    Update Lab611cal.php

--- Lab611cal.php
+++ Lab611cal.php
@@ -11,27 +11,36 @@
   $fn = $_GET ['fn'];
 
   echo "<p>";
-  echo "<b>Users input data</b></br>";
+  echo "<b> Users input data </b></br>";
   echo "Student Name : <b>$name</b></br>";
-  echo "Student Score : <b>$score</b></br>";
-  if ($score >= 80)
-  echo "Student Grade : <b>A </b><br/>";
-  elseif ($score >= 75)
-  echo "Student Grade : <b>B+ </b><br/>";
-  elseif ($score >= 70)
-  echo "Student Grade : <b>B </b><br/>";
-  elseif ($score >= 65)
-  echo "Student Grade : <b>C+ </b><br/>";
-  elseif ($score >= 60)
-  echo "Student Grade : <b>C </b><br/>";
-  elseif ($score >= 55)
-  echo "Student Grade : <b>D+ </b><br/>";
-  elseif ($score >= 50)
-  echo "Student Grade : <b>D </b><br/>";
+  echo "Points : <b>$pt</b></br>";
+  echo "Home work : <b>$hw</b></br>";
+  echo "Test : <b>$ts</b></br>";
+  echo "Assingment : <b>$as</b></br>";
+  echo "Midterm : <b>$mt</b></br>";
+  echo "Final : <b>$fn</b></br>";
+  $total = $pt+$hw+$ts+$as+$mt+$fn;
+
+  echo "Total Score  : <b>$total</b></br>";
+  if ($total>= 80)
+  echo "Result Grade : <b>A </b><br/>";
+  elseif ($total >= 75)
+  echo "Result Grade : <b>B+ </b><br/>";
+  elseif ($total >= 70)
+  echo "Result Grade : <b>B </b><br/>";
+  elseif ($total >= 65)
+  echo "Result Grade : <b>C+ </b><br/>";
+  elseif ($total >= 60)
+  echo "Result Grade : <b>C </b><br/>";
+  elseif ($total >= 55)
+  echo "Result Grade : <b>D+ </b><br/>";
+  elseif ($total >= 50)
+  echo "Result Grade : <b>D </b><br/>";
   else
-  echo "Student Grade : <b>F </b><br/>";
+  echo "Result Grade : <b>F </b><br/>";
+  echo "<br>";
 
-
+  echo"<a href='Lab611.html'><big> Back </big></a> ";
   ?>
 </body>
 </html>
