6e92e25804e9bb405fdef487064860fac9ab2084
IT5706021631042 <5706021631042@fitm.kmutnb.ac.th>
2016-02-24 12:02:23 +0700

    Update Lab612.php

--- Lab612.php
+++ Lab612.php
@@ -8,17 +8,17 @@
   <form method="post" action="lab612.php">
 <table align="center" border="1" width="400" bgcolor="#E6E6FA">
   <tr>
-    <td colspan="2" align="center">ทดสอบการใช้ Control Structure</td>
+    <td colspan="2" align="center"><big>ทดสอบการใช้ Control Structure<big></td>
     <tr>
       <tr>
-        <td align="right">Enter Row</td>
+        <td align="right">Enter Row : </td>
         <td><input type="text" name="row" size="15" value=""/></td>
       </tr>
-        <td align="right">Enter Col</td>
+        <td align="right">Enter Column : </td>
         <td><input type="text" name="col" size="15" value=""/></td>
       </tr>
       <tr>
-        <td align="center" colspan="2">
+        <td width="300" height="25" align="center" colspan="2">
           <input type="submit" value="OK"/>
           <input type="reset" value="CLEAR"/>
         </td>
@@ -29,7 +29,7 @@
 <?php
 $row = $_POST['row'];
 $col = $_POST['col'];
-echo "<table align='center' border='4' width='300'>";
+echo "<table align='center' border='3' width='300'>";
 for ($r=1; $r <= $row; $r++){
 echo "<tr>";
 for ($c=1; $c <= $col; $c++){
